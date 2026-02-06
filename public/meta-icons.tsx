04709533439914ba0a3235b8f972f569b7fe00e7
Captain AI <47535049+snjugunanjenga@users.noreply.github.com>
2026-02-06 15:30:52 +0000

    fix: Add favicon files and meta-icons.tsx to public directory

---
+++ public/meta-icons.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Favicon: React.FC = () => {
+  return (
+    <React.Fragment>
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+    </React.Fragment>
+  );
+};
+
+export default Favicon;
